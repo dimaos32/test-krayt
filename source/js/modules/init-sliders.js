3d2490aff77abd079162b4105bc699865990f475
Dima Os <dimaos@protonmail.com>
2021-03-30 11:09:14 +0300

    Выполняет секцию "Our Spaces"

--- source/js/modules/init-sliders.js
+++ source/js/modules/init-sliders.js
@@ -16,8 +16,34 @@ const initIntroducingSlider = () => {
   }
 };
 
+const initSpacesSlider = () => {
+  const spacesSlider = document.querySelector('.spaces__slider');
+
+  if (spacesSlider) {
+    let slider = new Swiper('.spaces__slider', {
+      speed: 400,
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 30,
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.spaces__slider-btn--next',
+        prevEl: '.spaces__slider-btn--prev',
+      },
+    });
+  }
+};
+
 const initSliders = () => {
   initIntroducingSlider();
+  initSpacesSlider();
 };
 
 export {initSliders};
